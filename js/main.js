b228087722db2ca0d187febba0d8f048b4f15bfa
Matias Altuna <altunamatias81@gmail.com>
2024-07-24 23:09:32 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,52 +1,44 @@
-/*let userName = prompt("Indiquenos su Nombre")
-
+let addForm = document.getElementById("addForm")
 const tasks = []
 
-taskOptions(userName)*/
 
 /* FUNCION PRINCIPAL */
-/*function taskOptions(user) {
-    let taskOption = confirm("Un gusto " + user + " ¿Desea agregar una tarea a su agenda?")
-    if (taskOption == true) {
-        addTask(user)
-        let taskOption = false
-    } else {
-        let otherOption = prompt("¿Desea eliminar, marcar como completada o revisar tareas? (1/2/3)")
-        switch (parseInt(otherOption)) {
-            case 1:
-                delTask()
-                break
-            case 2:
-                compTask()
-                break
-            case 3:
-                revTask()
-                break
-            default:
-                alert("Lo sentimos no es una opcion valida. Intentelo de nuevo")
-                break
-        }
+let getFormDates = function() {
+    let user = document.getElementById("userName").value
+    let ttl = document.getElementById("taskTtl").value
+    let date = document.getElementById("taskDate").value
+    let tim = document.getElementById("taskTime").value
+    let descrp = document.getElementById("taskDesc").value
+    if(!descrp) {
+        descrp = "No contiene descripción"
     }
-}*/
+    addTask(user, ttl, date, tim, descrp)
+}
 
+addForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    getFormDates()
+})
 
 
 /* FUNCIONES ESPECIFICAS */
-function addTask(user) {
-    do {
-        let taskName = prompt("¿Que desea agendar?").toUpperCase()
-        let taskDesc = prompt("Escribe una pequeña descripción").toUpperCase()
-        let taskDate = prompt("¿Cuando desea que se lo recordemos?").toUpperCase()
-        let task = {
-            name: taskName,
-            descrp: taskDesc,
-            date: taskDate,
-            creator: user,
-            completed: false
-        };
-        tasks.push(task);
-        console.log("Se agregó la tarea correctamente")
-    } while (confirm("¿Desea agregar otra?"))
+function addTask(user, ttl, date, tim, desc) {
+    let task = {
+        name: ttl,
+        descrp: desc,
+        date: date,
+        time: tim,
+        creator: user,
+        completed: false
+    };
+    tasks.push(task);
+    let container = document.createElement("div")
+    debugger
+    container.className = "taskCard task"
+    debugger
+    container.innerHTML =  `<h4>$[task.ttl]</h4>
+                            <p>$[task.date]<br>$[task.time]<br>$[task.descrip]<br>Creado por $[task.creator]<br>No completa</p>`
+    debugger
 }
 
 
